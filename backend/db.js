e97df0db19baf221c52c326b4c0ec7e1b1e1d5d0
Himanshu <hv65616@gmail.com>
2022-08-18 01:13:31 +0530

    changed the mongoURI string

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI = "mongodb://localhost:27017/signup";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
